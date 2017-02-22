2a7c8e48774310d71fc208998224a04edca1e613
Wayne Kenneth Seymour III <wayneseymour@gmail.com>
2017-02-22 11:20:16 -0500

    Started using character classes to capture more than one character.\nAlso, switched to using 'yarn'.

---
+++ character-classes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var str = `cat mat bat Hat ?at 0at`;
+var regex = /[cb]at/g
+
+/**
+ * @param  String str
+ * @param  RegExp regex
+ * @param  HTMLElement target
+ */
+const output = (str, regex, target) => {
+  target.innerHTML =
+    str.replace(regex, str =>
+      `<span>${str}</span>`
+    );
+}
+output(str, regex, document.querySelector('pre'));
